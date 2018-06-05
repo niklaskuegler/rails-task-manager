5bf3301b8e00752ad2c15cded2f1ddb68babab21
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-06-05 17:16:11 +0200

    Pretty nice work

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+
+  validates_presence_of :title, :details
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+  # get "tasks", to: "tasks#index", as: :tasks
+  # get "task/:id", to: "tasks#show", as: :task
+  # get "task", to: "task#index"
+  # post "task", to: "tasks#create"
+
 end
